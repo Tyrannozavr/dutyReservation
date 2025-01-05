e3cd628073962658c878f68b959b80c07609f034
dmiv <dmitriy40647274@gmail.com>
2025-01-05 17:46:34 +0300

    got data from telegram

---
+++ frontend/components/MiniApp.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { MainButton, useWebAppPopup } from 'vue-tg'
+const { showAlert } = useWebAppPopup()
+let tg = window.Telegram.WebApp
+
+</script>
+
+<template>
+  telegram webapp {{tg.initDataUnsafe}}
+  <MainButton text="Open alert" @click="() => showAlert('Hello!')" />
+</template>
