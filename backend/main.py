773f206075d05da828abcf4a768c95abd8d4d4ef
dmiv <dmitriy40647274@gmail.com>
2025-01-10 22:02:15 +0300

    database creation

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,8 @@
+import asyncio
+
 from fastapi import FastAPI
+
+from db.database import create_db_and_tables
 
 app = FastAPI()
 
@@ -8,3 +12,7 @@ from api.routers.api import router
 
 app.include_router(router)
 
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
