862ac73aea99824c1e556b68c59b7a68c537fbd6
dmiv <dmitriy40647274@gmail.com>
2025-01-24 19:50:52 +0300

    profile updated

--- frontend/components/Profile/Logout.vue
+++ frontend/components/Profile/Logout.vue
@@ -6,7 +6,7 @@ const logout = () => authStore.logout()
 </script>
 
 <template>
-<UIcon @click="logout" name="heroicons-outline:arrow-right-on-rectangle" class="w-8 h-8" />
+<UIcon @click="logout" name="heroicons-outline:arrow-right-on-rectangle" class="w-10 h-10" />
 </template>
 
 <style scoped>
--- frontend/components/Profile/User.vue
+++ frontend/components/Profile/User.vue
@@ -4,14 +4,23 @@ import {useUserStore} from "~/store/user";
 const userStore = useUserStore()
 </script>
 <template>
-  <div class="profile_container p-8 bg-white dark:bg-gray-900 rounded-lg shadow-md">
-    <h1 class="text-2xl font-bold mb-8 flex justify-between">Профиль <ProfileLogout class="ml-auto"/></h1>
-    <p class="text-lg mb-4">Добро пожаловать, {{userStore.first_name}} {{userStore.last_name}}</p>
-    <p class="text-lg mb-4">Юзернейм: {{userStore.username}}</p>
-    <p class="text-lg mb-4 flex flex-row">Аватар:
-      <UAvatar :src="userStore.photo_url" alt="avatar" size="md" class="ml-4" v-if="userStore.photo_url" />
-    </p>
-  </div>
+  <UContainer>
+    <UCard>
+      <template #header>
+        <h1 class="text-2xl font-bold flex">Профиль
+        <UAvatar :src="userStore.photo_url" alt="avatar" size="md" class="ml-auto" v-if="userStore.photo_url" />
+          <ProfileLogout class="ml-6"/>
+        </h1>
+      </template>
+    <UCard>
+      <p class="text-lg mb-4">Добро пожаловать, {{userStore.first_name}} {{userStore.last_name}}</p>
+      <p class="text-lg mb-4">Юзернейм: {{userStore.username}}</p>
+      <p class="text-lg mb-4 flex flex-row justify-between">Аватар:
+
+      </p>
+    </UCard>
+    </UCard>
+  </UContainer>
 </template>
 
 <style scoped>
