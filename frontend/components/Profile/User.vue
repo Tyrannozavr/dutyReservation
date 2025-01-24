7e52dd2d5b222f8f0c85c13ce3684ae3ff56bd06
dmiv <dmitriy40647274@gmail.com>
2025-01-24 19:34:38 +0300

    profile

--- frontend/components/Profile/User.vue
+++ frontend/components/Profile/User.vue
@@ -5,11 +5,15 @@ const userStore = useUserStore()
 </script>
 <template>
   <div class="profile_container p-8 bg-white dark:bg-gray-900 rounded-lg shadow-md">
-    <h1 class="text-2xl font-bold mb-4 flex justify-between">Профиль <ProfileLogout class="ml-auto"/></h1>
-
-    <p class="text-lg">Добро пожаловать, {{userStore.first_name}}</p>
+    <h1 class="text-2xl font-bold mb-8 flex justify-between">Профиль <ProfileLogout class="ml-auto"/></h1>
+    <p class="text-lg mb-4">Добро пожаловать, {{userStore.first_name}} {{userStore.last_name}}</p>
+    <p class="text-lg mb-4">Юзернейм: {{userStore.username}}</p>
+    <p class="text-lg mb-4 flex flex-row">Аватар:
+      <UAvatar :src="userStore.photo_url" alt="avatar" size="md" class="ml-4" v-if="userStore.photo_url" />
+    </p>
   </div>
 </template>
+
 <style scoped>
 
 </style>
