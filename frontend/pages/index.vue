fc494a9d461be78665121bc7734030a17b029197
dmiv <dmitriy40647274@gmail.com>
2025-02-05 19:45:33 +0300

    главная страница

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,14 +1,43 @@
-<script setup lang="ts">
-const tg = ref(null)
-const user = ref(null)
-
-onMounted(() => {
-   // tg.value = window.Telegram?.WebApp
-   // user.value = tg.value?.initDataUnsafe?.user
-})
-</script>
 <template>
-  tg is {{tg}}
+  <UContainer class="mx-auto p-4">
+    <UCard class="shadow-md rounded-lg">
+      <UCard>
+        <template #header>
+          <!--            <img src="/logo.png" alt="Logo" class="w-32 h-32 mx-auto mb-4">-->
+          <h1 class="text-2xl font-bold mb-4">Добро пожаловать в Duty Reservation!</h1>
+
+        </template>
+        <p class="mb-6">
+          Duty Reservation - это веб-приложение, которое позволяет создавать дежурства и управлять ими,
+          делиться доступом с людьми, которые могут бронировать заданные вами даты
+        </p>
+      </UCard>
+
+      <h2 class="text-xl font-semibold mb-2 mt-2">Как использовать Duty Reservation?</h2>
+      <p class="mb-4">
+        Для начала работы с Duty Reservation, вам необходимо создать аккаунт или войти в существующий.
+      </p>
+      <p class="mb-4">
+        После этого вы можете
+        <NuxtLink to="/created" class="text-blue-500 hover:text-blue-700 font-medium">
+          Создать
+        </NuxtLink>
+        свое дежурство или
+        <NuxtLink to="/store" class="text-blue-500 hover:text-blue-700 font-medium">
+          присоединиться
+        </NuxtLink>
+        к существующему
+      </p>
+      Кстати, если вы откроете этот сайт через приложение
+      <NuxtLink to="https://t.me/DutyReservationBot/reservationpage"
+                class="font-medium text-blue-500 hover:text-blue-700">
+        Telegram
+      </NuxtLink>
+      по этой ссылке
+
+      , то сможете пользоваться приложением без дополнительной авторизации.
+    </UCard>
+  </UContainer>
 </template>
-<style scoped>
-</style>
+<script setup lang="ts">
+</script>
